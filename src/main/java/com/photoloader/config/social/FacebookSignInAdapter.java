cbd85b6a12b1cc120e48c35be5b68d8f9419f43b
akolhun <mag-ua@ukr.net>
2022-03-11 15:49:09 +0200

    add social sign in functionality

--- src/main/java/com/photoloader/config/social/FacebookSignInAdapter.java
+++ src/main/java/com/photoloader/config/social/FacebookSignInAdapter.java
@@ -19,7 +19,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-//@Component
 @Slf4j
 public class FacebookSignInAdapter implements SignInAdapter {
     private final Set<UserData> authorizedUsers;
