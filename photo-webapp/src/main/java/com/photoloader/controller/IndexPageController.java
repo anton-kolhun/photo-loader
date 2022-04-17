ab9d5f8da40bc62ec16e98e4cb327e48b43c6c01
Anton Kolhun <mag-ua@ukr.net>
2022-04-17 10:07:38 +0300

    segregate ui for desktop and mobile (#12)

--- photo-webapp/src/main/java/com/photoloader/controller/IndexPageController.java
+++ photo-webapp/src/main/java/com/photoloader/controller/IndexPageController.java
@@ -1,25 +1,31 @@
 package com.photoloader.controller;
 
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
 public class IndexPageController {
 
-    @GetMapping
-    public String index() {
-        return "index";
+  @GetMapping
+  public String index(@RequestHeader Map<String, String> headers) {
+    String indexPage = "index";
+    if (headers.get("user-agent").toLowerCase().contains("mobile")) {
+      indexPage = "index_mobile";
     }
+    return indexPage;
+  }
 
-    @GetMapping("login")
-    public String login() {
-        return "login";
-    }
+  @GetMapping("login")
+  public String login() {
+    return "login";
+  }
 
-    @GetMapping("policy")
-    public String policy() {
-        return "policy";
-    }
+  @GetMapping("policy")
+  public String policy() {
+    return "policy";
+  }
 }
