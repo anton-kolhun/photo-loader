917ca1524b1c3917c8721a203829962f891cbda7
akolhun <antonkol@wix.com>
2023-10-24 11:27:10 +0300

    add integration with ntfy.sh (#19)

--- photo-webapp/src/main/java/com/photoloader/config/social/fb/FacebookSignInAdapter.java
+++ photo-webapp/src/main/java/com/photoloader/config/social/fb/FacebookSignInAdapter.java
@@ -6,11 +6,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.web.SignInAdapter;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.request.NativeWebRequest;
 
 import java.io.IOException;
@@ -23,53 +25,72 @@ import java.util.Set;
 @Slf4j
 public class FacebookSignInAdapter implements SignInAdapter {
 
-  private final Set<UserData> authorizedUsers;
+    private final Set<UserData> authorizedUsers;
+
+    private final RestTemplate restTemplate;
+
+    private final String ntfyUrl;
 
 
-  public FacebookSignInAdapter(ObjectMapper objectMapper, String authorizedUsersPath) {
-    this.authorizedUsers = loadAuthorizedUsers(objectMapper, authorizedUsersPath);
-  }
-
-  @Override
-  public String signIn(String localUserId, Connection<?> connection, NativeWebRequest request) {
-    log.info("signed in via fb as name = {}, id= {} ", connection.getDisplayName(),
-        connection.getKey().getProviderUserId());
-    if (!authorizedUsers.contains(new UserData(connection.getKey().getProviderUserId()))) {
-      SecurityContextHolder.getContext()
-          .setAuthentication(new UsernamePasswordAuthenticationToken(connection, null,
-              Arrays.asList(new SimpleGrantedAuthority("ROLE_PUBLIC"))));
-    } else {
-      SecurityContextHolder.getContext()
-          .setAuthentication(new UsernamePasswordAuthenticationToken(connection, null,
-              Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"))));
-    }
-    return null;
-  }
-
-  private Set<UserData> loadAuthorizedUsers(ObjectMapper objectMapper, String authorizedUsersPath) {
-    try {
-      byte[] usersBinary = Files.readAllBytes(Paths.get(authorizedUsersPath));
-      UserData[] authorizedUsers = objectMapper.readValue(usersBinary, UserData[].class);
-      return new HashSet<>(Arrays.asList(authorizedUsers));
-    } catch (IOException e) {
-      throw new RuntimeException("error occurred while loading users from file", e);
-    }
-  }
-
-  @Getter
-  @Setter
-  @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-  @NoArgsConstructor
-  public static class UserData {
-
-    @EqualsAndHashCode.Include
-    private String id;
-
-    private String name;
-
-    public UserData(String id) {
-      this.id = id;
+    public FacebookSignInAdapter(ObjectMapper objectMapper, String authorizedUsersPath, RestTemplate restTemplate,
+                                 String ntfyUrl) {
+        this.authorizedUsers = loadAuthorizedUsers(objectMapper, authorizedUsersPath);
+        this.restTemplate = restTemplate;
+        this.ntfyUrl = ntfyUrl;
     }
 
-  }
+    @Override
+    public String signIn(String localUserId, Connection<?> connection, NativeWebRequest request) {
+        logSignIn(connection);
+        if (!authorizedUsers.contains(new UserData(connection.getKey().getProviderUserId()))) {
+            SecurityContextHolder.getContext()
+                    .setAuthentication(new UsernamePasswordAuthenticationToken(connection, null,
+                            Arrays.asList(new SimpleGrantedAuthority("ROLE_PUBLIC"))));
+        } else {
+            SecurityContextHolder.getContext()
+                    .setAuthentication(new UsernamePasswordAuthenticationToken(connection, null,
+                            Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"))));
+        }
+        return null;
+    }
+
+    private void logSignIn(Connection<?> connection) {
+        String msg = String.format("signed in via fb as name = %s, id= %s ", connection.getDisplayName(),
+                connection.getKey().getProviderUserId());
+        log.info(msg);
+        try {
+            HttpEntity<String> requestEntity = new HttpEntity<>(msg);
+            String response = restTemplate.postForObject(ntfyUrl, requestEntity, String.class);
+            log.info("ntfy response: {}", response);
+        } catch (Exception e) {
+            log.warn("Could not send msg to ntfy.sh", e);
+        }
+    }
+
+    private Set<UserData> loadAuthorizedUsers(ObjectMapper objectMapper, String authorizedUsersPath) {
+        try {
+            byte[] usersBinary = Files.readAllBytes(Paths.get(authorizedUsersPath));
+            UserData[] authorizedUsers = objectMapper.readValue(usersBinary, UserData[].class);
+            return new HashSet<>(Arrays.asList(authorizedUsers));
+        } catch (IOException e) {
+            throw new RuntimeException("error occurred while loading users from file", e);
+        }
+    }
+
+    @Getter
+    @Setter
+    @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+    @NoArgsConstructor
+    public static class UserData {
+
+        @EqualsAndHashCode.Include
+        private String id;
+
+        private String name;
+
+        public UserData(String id) {
+            this.id = id;
+        }
+
+    }
 }
